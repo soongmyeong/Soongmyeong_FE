3cf90339fd0ed14dd603550a214070a032741f09
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-06-30 23:12:26 +0900

    [feat] Route Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SingIn';
+import Post from './pages/Posts';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/post" element={<Post/>}>
+        <Route path=":id"/>
+      </Route>
+    </Routes>
   );
 }
 
